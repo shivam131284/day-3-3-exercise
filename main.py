028573868610f0ff443a698dd038051a324eac41
crispwand <55968849+shivam131284@users.noreply.github.com>
2021-08-20 07:25:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# 🚨 Don't change the code below 👇
+year = int(input("Which year do you want to check? "))
+# 🚨 Don't change the code above 👆
+if year % 4 == 0:
+  if year % 100 ==0:
+    if year % 400 ==0:
+      print("leap year")
+    else:
+      print(" leap year")
+  else:
+      print(" a leap ")  
+else :
+  print("not a leap year:")
+
+
+  
+  
+
+#Write your code below this line 👇
+
+
+
